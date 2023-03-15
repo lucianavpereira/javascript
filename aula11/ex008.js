a1b7c1c69a4ca18d3ddd179ebb3ded9650c88b6c
Luciana V Pereira <115096459+lucianavpereira@users.noreply.github.com>
2023-03-15 18:53:21 -0300

    Condições em js

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+console.log('O console funcionou corretamente!')
+var vel = 55.4
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if (vel > 70) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Use sempre o cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,14 @@
+var pais = 'França'
+console.log(`Vivendo em ${pais}!`)
+
+// if (pais == 'Brasil') {
+//     console.log(`Você é Brasileiro!`)
+// } else {
+//     console.log(`Você é Estrangeiro!`)
+// }
+
+if (pais != 'Brasil') {
+    console.log(`Você é Estrangeiro!`)
+} else {
+    console.log(`Você é Brasileiro!`)
+}
