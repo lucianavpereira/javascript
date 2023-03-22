38daf06b367336c620e7c011f80be13537ca84a4
Luciana V Pereira <115096459+lucianavpereira@users.noreply.github.com>
2023-03-22 12:54:03 -0300

    exercícios aula 13 html/css/js

--- aula13/script.js
+++ aula13/script.js
@@ -1,1 +1,22 @@
-alert ('olá!!!')
+function carregar() {
+    var dia = window.document.getElementById('mensagem')
+    var foto = window.document.getElementById('foto')
+    var data = new Date()
+    var hora = data.getHours()
+    // var hora = 13
+    dia.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora <12) {
+        // Bom dia
+        foto.src = 'fotomanha.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora < 18) {
+        // Boa tarde
+        foto.src = 'fototarde.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        // Boa noite
+        foto.src = 'fotonoite.png'
+        document.body.style.background = '#515154'
+    }
+
+}
