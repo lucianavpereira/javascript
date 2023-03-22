87f0da8a15f650ea77725a0772a0916d66e44f08
Luciana V Pereira <115096459+lucianavpereira@users.noreply.github.com>
2023-03-22 15:20:50 -0300

    exer js/html/css

--- aula13/script.js
+++ aula13/script.js
@@ -2,21 +2,21 @@ function carregar() {
     var dia = window.document.getElementById('mensagem')
     var foto = window.document.getElementById('foto')
     var data = new Date()
-    var hora = data.getHours()
-    // var hora = 13
+    // var hora = data.getHours()
+    var hora = 19
     dia.innerHTML = `Agora são ${hora} horas`
     if (hora >= 0 && hora <12) {
         // Bom dia
         foto.src = 'fotomanha.png'
-        document.body.style.background = '#e2cd9f'
+        document.body.style.background = '#FFE500'
     } else if (hora >= 12 && hora < 18) {
         // Boa tarde
         foto.src = 'fototarde.png'
-        document.body.style.background = '#b9846f'
+        document.body.style.background = '#CC8D1A'
     } else {
         // Boa noite
         foto.src = 'fotonoite.png'
-        document.body.style.background = '#515154'
+        document.body.style.background = '#01153D'
     }
 
 }
