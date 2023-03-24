cfd2ec71ea08309bc85db3b6c3cc9fef8c865d8e
Luciana V Pereira <115096459+lucianavpereira@users.noreply.github.com>
2023-03-24 13:23:39 -0300

    update exerc 13-2 js

--- aula13-2/script.js
+++ aula13-2/script.js
@@ -1,3 +1,27 @@
 function verificar() {
     
+    var data = new Date()
+    var ano = data.getFullYear()
+    var nasceu = window.document.querySelector('#nasc')
+    // var idadeatual = Number(ano.value)
+    var anonas = Number(nasceu.value)
+    var idade = ano - anonas
+    var pessoa = window.document.querySelector('#foto')
+    var femi = window.document.querySelector('#fem')
+    var macu = window.document.querySelector('#mas')
+    
+    var res = window.document.querySelector('#resul')
+    res.innerHTML = (`Sua idade é ${idade} anos!!! :)`)
+
+
+    if (anonas == 0 || anonas > ano) {
+        // window.alert('[ERRO] Ano inválido.')
+        res.innerHTML = ('[ERRO] Ano inválido.')
+    } else if (ano - 12 > anonas && femi) {
+        pessoa.src = 'menina.png' 
+        
+    } 
+    
+    
 }
+
