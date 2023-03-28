b09207de8f228c38738422bd8c8d0b1e700aa3db
Luciana V Pereira <115096459+lucianavpereira@users.noreply.github.com>
2023-03-28 12:11:35 -0300

    Repetições em JS

---
+++ aula14/repeticao.js
@@ -0,0 +1,33 @@
+for(var c = 1;c <= 10;c++) {
+    console.log(c)
+}
+console.log('FIM!')
+
+
+
+
+// var c = 1
+// do {
+//     console.log('tudo legal?')
+//     c++
+// } while (c <= 8)
+
+
+
+// var c = 1
+// while (c <= 10) {
+//     //console.log('tudo legal?')
+//     console.log(`Legal ${c}`)
+//     c++ // c = c+1
+// }
+
+
+
+// console.log('tudo legal?')
+// console.log('tudo legal?')
+// console.log('tudo legal?')
+// console.log('tudo legal?')
+// console.log('tudo legal?')
+// console.log('tudo legal?')
+// console.log('tudo legal?')
+// console.log('tudo legal?')
