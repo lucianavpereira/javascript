b92d9b675b3e6fd9eea4671969c0631ee3703632
Luciana V Pereira <115096459+lucianavpereira@users.noreply.github.com>
2023-03-29 18:53:05 -0300

    exerc 14

--- aula14-2/script.js
+++ aula14-2/script.js
@@ -1,25 +1,23 @@
 function calcular() {
     var num = window.document.querySelector('#num')
     var nump = Number(num)
-    var tb = window.document.getElementsByClassName('.opt')
+    var tb = window.document.getElementsByName('tabuada')
     var tbp = Number(tb)
     var calc = Number(tbp * nump)
     var res = window.document.querySelector('#res')
 
-    if (tb >= 0 && tb <= 10) {
-        while (tb >= 0 && tb <= 10) {
-            var calc = tbp*nump
-            tb++
-        
+    if (tbp >= 0 && tbp <= 10) {
+        for(var tbp = 0; tbp <=10; tbp++){
+            window.alert(calc)
         }
+
+        // while (tb >= 0 && tb <= 10) {
+        //     var calc = tbp*nump
+        //     tb++
+        
+        // }
     }
     res.innerHTML = (`A tabuada de  ${tbp} x ${nump} = ${calc}`)
-
-
-    
-
-
-
 
 
 
