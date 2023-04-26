0f6875ef0427a25f3d886238c1f67486f6ed150e
Luciana V Pereira <115096459+lucianavpereira@users.noreply.github.com>
2023-04-26 15:06:18 -0300

    exercício 14-1 concluído

--- aula14-1/script.js
+++ aula14-1/script.js
@@ -2,18 +2,27 @@ function contar() {
     var inicio = Number(window.document.querySelector('#inicio').value)
     var final = Number(window.document.querySelector('#fim').value)
     var passo = Number(window.document.querySelector('#pulo').value)
-
     var res = window.document.querySelector('#res')
 
-    if (inicio <= 0 || final <= 0 || passo <=0) {
-        window.alert('Valor inválido!')
+    if (inicio <= 0 || final <= 0) {
+        window.alert('[ERRO]Valor inválido!')
+    } else {
+            res.innerHTML = 'Contando....  <br>'
+            if (passo <= 0) {
+                window.alert('Passo não informado! Considerando Passo 1.')
+                passo = 1
+            }
+            if(inicio <= final) {
+            // contagem crescente
+            for(var c = inicio; c <= final; c += passo){
+                res.innerHTML += `   ${c}  \u{1F603}`
+            }
         } else {
-        for(var c = inicio; c <= final; c += passo){
-            res.innerHTML += `   ${c}`
+            //contagem regressiva
+            for(var c = inicio; c >= final; c -= passo){
+                res.innerHTML += `   ${c}  \u{1F603}`
+            }           
         }
-        }
-
-
-                  
-       
+        res.innerHTML += `  \u{1F3C1}`        
+    }   
 }
