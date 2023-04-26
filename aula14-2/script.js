219c51c22332b197bb9941c34061071b8fd6aa23
Luciana V Pereira <115096459+lucianavpereira@users.noreply.github.com>
2023-04-26 19:35:46 -0300

    exercício 14-2 concluído

--- aula14-2/script.js
+++ aula14-2/script.js
@@ -1,24 +1,11 @@
 function calcular() {
-    var num = window.document.querySelector('#num')
-    var nump = Number(num)
-    var tb = window.document.getElementsByName('tabuada')
-    var tbp = Number(tb)
-    var calc = Number(tbp * nump)
-    var res = window.document.querySelector('#res')
-
-    if (tbp >= 0 && tbp <= 10) {
-        for(var tbp = 0; tbp <=10; tbp++){
-            
-        }
-
-        // while (tb >= 0 && tb <= 10) {
-        //     var calc = tbp*nump
-        //     tb++
-        
-        // }
+    var num = Number(window.document.querySelector('#num').value)
+    var res = window.document.querySelector('#tabuada')
+    res.innerHTML = ''
+    for (var c = 1; c <= 10; c++) {
+        var item = document.createElement('option') 
+        item.text = `${num} X ${c} = ${num*c}`
+        // item.value = `res${c}`
+        res.appendChild(item)
     }
-    res.innerHTML = (`A tabuada de  ${tbp} x ${nump} = ${calc}`)
-
-
-
 }
